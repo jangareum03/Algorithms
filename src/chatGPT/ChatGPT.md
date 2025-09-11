3d578e162b2a2a61601007511ac01e944eb58952
jangareum03 <jangreum03@gmail.com>
2025-09-11 21:43:14 +0900

    모든 스레드 종료 확인

--- src/chatGPT/ChatGPT.md
+++ src/chatGPT/ChatGPT.md
@@ -1,6 +1,7 @@
 # ChatGPT 문제 풀이
 
- |  유형   |   제목   |    클래스 이름    |
-|:-----:|:------:|:------------:|
-| 디자인패턴 | 로그 작성  | RecordLogger |
-|  스레드  | 문자열 출력 | StringThread |
+ |  유형   |   제목   |    클래스 이름    |                 풀이                 |
+|:-----:|:------:|:------------:|:----------------------------------:|
+| 디자인패턴 | 로그 작성  | RecordLogger | [링크](https://areum.tistory.com/36) |
+|  스레드  | 문자열 출력 | StringThread | [링크](https://areum.tistory.com/37) |
+|  스레드  | 스레드종료  |  JoinThread  | [링크](https://areum.tistory.com/39) |
---
+++ src/chatGPT/JoinThread.java
@@ -0,0 +1,35 @@
+package chatGPT;
+
+/**
+	멀티 스레드 문제 - 모든 스레드 종료 확인
+ */
+public class JoinThread {
+	public static void main(String[] args) throws InterruptedException {
+		Thread numberThread = new NumberThread();
+		Thread starThread = new Thread(new StarThread());
+
+		numberThread.start();
+		starThread.start();
+
+		System.out.println("Done!");
+
+	}
+}
+
+class NumberThread extends Thread{
+	@Override
+	public void run() {
+		for(int i=1; i<=5; i++) {
+			System.out.println(Thread.currentThread().getName() + " - " + i);
+		}
+	}
+}
+
+class StarThread implements Runnable {
+	@Override
+	public void run() {
+		for(int i=0; i<5; i++) {
+			System.out.println(Thread.currentThread().getName() + " - ★");
+		}
+	}
+}
