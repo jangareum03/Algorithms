a93b8c348e468ccf9cb7792a62dfce2f32dc0c62
jangareum03 <jangreum03@gmail.com>
2025-10-13 18:21:44 +0900

    [추가] 주석 추가

--- src/chatGPT/sort/selection/RankSort.java
+++ src/chatGPT/sort/selection/RankSort.java
@@ -12,16 +12,23 @@ public class RankSort {
 		System.out.println(Arrays.toString(topScores));
 	}
 
+	/**
+	 * 점수 높은 순으로 n번째까지만 배열로 담은 후 반환하는 메서드
+	 *
+	 * @param n			상위 n번째
+	 * @param scores	점수 배열
+	 * @return	상위 n번째 점수만 담은 배열
+	 */
 	private static int[] descTopNByScores(int n, int[] scores) {
-		int[] copyArr = Arrays.copyOf(scores, scores.length);
+		int[] copyArr = Arrays.copyOf(scores, scores.length);	//원본 배열 복사
 		int[] top = new int[n];
 
-		for( int i=0; i<n; i++ ) {
+		for( int i=0; i<n; i++ ) {		//상위 n번째만 필요하기에 반복문을 n까지만 진행
 			int  maxIndex = i;
 
 			for( int j=i; j<copyArr.length; j++ ) {
 				if( copyArr[maxIndex] < copyArr[j] ) {
-					maxIndex = j;
+					maxIndex = j;	//최대값 인덱스를 큰값의 인덱스로 변경
 				}
 			}
 
